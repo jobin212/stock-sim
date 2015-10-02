752ad358e1999b1dae074bffe880c0250a8572bb
joe <jobin212@gmail.com>
2015-10-02 01:39:29 -0400

    Added marketTest.cpp file

---
+++ marketTest.cpp
@@ -0,0 +1,19 @@
+//Joseph Tobin jobin212@github.com
+
+
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+	double priceA, priceB, priceC = 10.0, 25.0 60.0;
+
+
+
+	Stock * AAAA = new Stock("AAAA", priceA);
+
+	Stock * BBBB = new Stock("BBBB", 0.75);
+
+	Stock * CCCC = new Stock("CCCC", )
+}
